af439128a4f5f16fffa1ca887a4201c835320640
Igreeda <82223725+Igggor@users.noreply.github.com>
2023-06-02 12:45:39 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -58,14 +58,72 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 });
 
 
-let but = document.getElementById("bubu");
-but.addEventListener("click", function(){
-    but.setText("ПРИВЕТ");
-});
 
-let drop1 = document.getElementById("Home_model");
-let drop2 = document.getElementById("mont_type");
-let drop3 = document.getElementById("obr_type");
+document.getElementById("bubu").onclick = function(){
+    let data = {};
+
+    var e = document.getElementById("Home_model");
+    var text = e.options[e.selectedIndex].text;
+    data["Home_model"] = text
+    
+    e = document.getElementById("mont_type");
+    text = e.options[e.selectedIndex].text;
+    data["mont_type"] = text;
+    
+    e = document.getElementById("obr_type");
+    text = e.options[e.selectedIndex].text;
+    data["obr_type"] = text;
+    
+
+    data["user"] = document.getElementById("user").value;
+    
+    data["worker"] = document.getElementById("worker").value;
+
+    data["mounter"] = document.getElementById("mounter").value;
+
+    data["adress"] = document.getElementById("adress").value;
+    
+    data["obr_type"] = document.getElementById("obr_type").value;
+
+    data["comm"] = document.getElementById("comm").value;
+
+
+    document.getElementById("drive_date").addEventListener("change", function() {
+        var input = this.value;
+        data["drive_date"] = input; });
+    
+    
+    document.getElementById("KS_date").addEventListener("change", function() {
+        var input = this.value;
+        data["KS_date"] = input; });
+
+    
+    
+// Дальще не работает(((
+    // alert(data["comm"])
+    var jsonData = JSON.stringify(data);
+
+// Создание объекта для работы с файлами
+    var fs = require('fs');
+
+// Запись данных в файл
+    fs.writeFile('data.json', jsonData, 'utf8', function(err) {
+      if (err) {
+        console.log('Ошибка записи файла:', err);
+      } else {
+        console.log('Данные успешно записаны в файл.');
+      }
+    });
+
+    this.style.backgroundColor = "Red"
+    alert(data)
+    
+    
+  }
+
+// let drop1 = document.getElementById("Home_model");
+// let drop2 = document.getElementById("mont_type");
+// let drop3 = document.getElementById("obr_type");
 
 
 // Открыть файл
