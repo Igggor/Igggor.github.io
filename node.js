088d985befa3bce3457c5ff6fc74ed0962462ea3
Igreeda <82223725+Igggor@users.noreply.github.com>
2023-06-02 13:13:00 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -58,50 +58,6 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData("Ваша форма отправлена");
 });
 
-
-
-document.getElementById("bubu").onclick = function(){
-    let data = {};
-
-    var e = document.getElementById("Home_model");
-    var text = e.options[e.selectedIndex].text;
-    data["Home_model"] = text
-    
-    e = document.getElementById("mont_type");
-    text = e.options[e.selectedIndex].text;
-    data["mont_type"] = text;
-    
-    e = document.getElementById("obr_type");
-    text = e.options[e.selectedIndex].text;
-    data["obr_type"] = text;
-    
-
-    data["user"] = document.getElementById("user").value;
-    
-    data["worker"] = document.getElementById("worker").value;
-
-    data["mounter"] = document.getElementById("mounter").value;
-
-    data["adress"] = document.getElementById("adress").value;
-    
-    data["obr_type"] = document.getElementById("obr_type").value;
-
-    data["comm"] = document.getElementById("comm").value;
-
-
-    document.getElementById("drive_date").addEventListener("change", function() {
-        var input = this.value;
-        data["drive_date"] = input; });
-    
-    
-    document.getElementById("KS_date").addEventListener("change", function() {
-        var input = this.value;
-        data["KS_date"] = input; });   
-
-        tg.sendData(data); 
-     
-  }
-
 tg.MainButton.onClick(callback) = function(){
     let data = {};
 
@@ -140,7 +96,7 @@ tg.MainButton.onClick(callback) = function(){
         var input = this.value;
         data["KS_date"] = input; });   
         
-        tg.sendData(data); 
+        tg.sendData("data"); 
     }
 
 
