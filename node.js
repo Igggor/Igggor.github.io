e3bce9465621d330a8aa10a7510f0b5c951d90a9
Igreeda <82223725+Igggor@users.noreply.github.com>
2023-06-05 09:31:11 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -12,41 +12,53 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     var e = document.getElementById("Home_model");
     var text = e.options[e.selectedIndex].text;
     data["Home_model"] = text
+    alert( data["Home_model"]);
     
     e = document.getElementById("mont_type");
     text = e.options[e.selectedIndex].text;
     data["mont_type"] = text;
+    alert( data["mont_type"]);
 
     e = document.getElementById("obr_type");
     text = e.options[e.selectedIndex].text;
     data["obr_type"] = text;
+    alert( data["obr_type"]);
 
     text = document.getElementById("user").val();
     data["user"] = text;
+    alert( data["user"]);
 
     text = document.getElementById("worker").val();
     data["worker"] = text;
+    alert( data["worker"]);
 
     text = document.getElementById("mounter").val();
     data["mounter"] = text;
+    alert( data["mounter"]);
 
     text = document.getElementById("adress").val();
     data["adress"] = text;
+    alert( data["adress"]);
 
     text = document.getElementById("obr_type").val();
     data["obr_type"] = text;
+    alert( data["obr_type"]);
 
     text = document.getElementById("comm").val();
     data["comm"] = text;
+    alert( data["comm"]);
     document.getElementById("drive_date").addEventListener("change", function() {
         var input = this.value;
         data["drive_date"] = input; });
+    alert( data["drive_date"]);
     
     
     tg.MainButton.color = "#FFFFFF";
     document.getElementById("KS_date").addEventListener("change", function() {
         var input = this.value;
         data["KS_date"] = input; });
+    alert( data["KS_date"]);
+    
     
 
 
@@ -55,11 +67,12 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     //     if (err) throw err;
     //     console.log("All right!");
     // });
-
+    alert("colors...");
     
     tg.sendData("Ваша форма отправлена");
     tg.MainButton.color = "#000000";
     tg.close();
+    alert("finished");
     
 
 });
