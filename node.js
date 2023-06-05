77093ecdc8e1d16b52d5b118268135502d88c258
Igreeda <82223725+Igggor@users.noreply.github.com>
2023-06-05 10:36:35 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -51,63 +51,10 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
         data["KS_date"] = input; });
     
     
-    tg.sendData(data);
+    // tg.sendData(data);
+    tg.sendData("S");
     alert("finished");
     tg.close();
     
 
 });
-
-// tg.MainButton.onClick(callback) = function(){
-//     let data = {};
-
-//     var e = document.getElementById("Home_model");
-//     var text = e.options[e.selectedIndex].text;
-//     data["Home_model"] = text
-    
-//     e = document.getElementById("mont_type");
-//     text = e.options[e.selectedIndex].text;
-//     data["mont_type"] = text;
-    
-//     e = document.getElementById("obr_type");
-//     text = e.options[e.selectedIndex].text;
-//     data["obr_type"] = text;
-    
-
-//     data["user"] = document.getElementById("user").value;
-    
-//     data["worker"] = document.getElementById("worker").value;
-
-//     data["mounter"] = document.getElementById("mounter").value;
-
-//     data["adress"] = document.getElementById("adress").value;
-    
-//     data["obr_type"] = document.getElementById("obr_type").value;
-
-//     data["comm"] = document.getElementById("comm").value;
-
-
-//     document.getElementById("drive_date").addEventListener("change", function() {
-//         var input = this.value;
-//         data["drive_date"] = input; });
-    
-    
-//     document.getElementById("KS_date").addEventListener("change", function() {
-//         var input = this.value;
-//         data["KS_date"] = input; });   
-//     let s = ""
-//     tg.sendData("data"); 
-//     tg.close()
-//     tg.sendData("some string that we need to send");
-//     tg.MainButton.color = "#FFFFFF";
-
-//     }
-
-
-// let drop1 = document.getElementById("Home_model");
-// let drop2 = document.getElementById("mont_type");
-// let drop3 = document.getElementById("obr_type");
-
-
-// Открыть файл
-// Получить из файла данные для дроплистов
