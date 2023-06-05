a195e73ae08254d254bc30bd27cba92b2c03bea2
Igreeda <82223725+Igggor@users.noreply.github.com>
2023-06-05 14:26:42 +0300

    Add files via upload

--- node.js
+++ node.js
@@ -43,13 +43,11 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     data["comm"] = document.getElementById("comm").value;
 
     document.getElementById("drive_date").addEventListener("change", function() {
-        var input = this.value;
-        var test = new Date(input);
-        alert(test);
+        var input = this.valueAsDate;
         data["drive_date"] = input; });
     
     document.getElementById("KS_date").addEventListener("change", function() {
-        var input = this.value;
+        var input = this.valueAsDate;
         data["KS_date"] = input; });
     
     
